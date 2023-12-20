bfe5a26f11f896c9194ef6209af6022baa1b25f0
Serhat Sel <selserhat01@gmail.com>
2023-12-20 22:05:30 +0300

    state onClick onChange map

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,59 @@ import Text from "./components/Text";
 function App() {
   // let name = "serhat";
 
-  const [name, setName] = useState("react");
+  // const [name, setName] =  // const clickFunc = () => {
+  //   console.log("Click işlemi yapıldı");
+  //   setName("react çok değişti ");
+  // };useState("react");
 
-  const clickFunc = () => {
-    console.log("Click işlemi yapıldı");
+  // const [count, setCount] = useState(0);
+
+  // const decrement = () => {
+  //   setCount(count - 1);
+  // };
+
+  const [name, setName] = useState([null]);
+  const nameFunc = (e) => {
+    setName(e.target.value);
   };
 
+  console.log("Name -->", name);
+
+  const [data, setData] = useState([]);
+
+  const dataFunc = () => {
+    setData((prev) => [...prev, name]);
+  };
+  console.log(data);
   return (
     <>
-      <div onClick={clickFunc}>{name}</div>
+      {/* <div onClick={clickFunc}>{name}</div>
       <div>{name}</div>
-      <div>{name}</div>
-      <Text number="1" name={"react"} />
+      <div>{name}</div> */}
+      {/* <Text number="1" name={"react"} />
       <Text number="2" />
-      <Text number="33" />
-      <Button name="Arttır" />
-      <Button name="azalt" />
+      <Text number="33" /> */}
+      {/* <Button name="Arttır" /> */}
+      {/* <Button name="azalt" /> */}
+      {/* <div>{count}</div> */}
+      {/* <Button name={"Arttır"} onClick={() => setCount(count + 1)} /> */}
+      {/* <button onClick={() => setCount(count + 1)}>arttır</button> */}
+      {/* <button onClick={decrement}>azalt</button> */}
+
+      <input
+        type="text"
+        placeholder="enter name"
+        // onChange={(e) => {
+        //   setName(e.target.value);
+        // }}
+        onChange={nameFunc}
+      />
+      <button onClick={dataFunc}>tıkla</button>
+      <div>
+        {data.map((value, i) => (
+          <div key={i}>{value}</div>
+        ))}
+      </div>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ name }) => {
+const Button = ({ name, onClick }) => {
   return (
     <>
-      <button>{name}</button>
+      <button onClick={onClick}>{name}</button>
     </>
   );
 };
